b143b943e86f66000b304f0bbbc2b60177baa2ee
Suyash Nandurkar <suyashnandurkar53@gmail.com>
2023-10-20 01:23:56 +0530

    Chore: Updated

--- app.js
+++ app.js
@@ -8,28 +8,38 @@ var txt_Output = document.querySelector("#txtOutput");
 
 
 //processing
-function isLeapYear(intputYear) {
-    if ((intputYear % 4 === 0 && intputYear % 100 !== 0) || intputYear % 400 === 0) {
-        return true;
-    } else {
-        return false;
+function isBirthPrime(inputDate) {
+    for(var i=2; i<=inputDate-1;i++){
+        if (inputDate % i === 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
+    
 }
 
 function clickEventListener() {
     var inputName = txt_Name.value;
-    var inputData = txt_Data.value;
+    var inputDate = txt_Data.value;
     var inputMonth = txt_Month.value;
     var inputYear = txt_Year.value;
 
    
-    if (isLeapYear(inputYear)) {
-        //output
-        var outputTxtY = ` Hey! ${inputName}\n DOB: ${inputData}/${inputMonth}/${inputYear}\n You are born on leap year`;
-        txt_Output.innerText = outputTxtY;
-    } else {
-        var outputTxtN = ` Hey! ${inputName}\n DOB: ${inputData}/${inputMonth}/${inputYear}\n You aren't born on leap year`;
-        txt_Output.innerText = outputTxtN;
+    if(inputDate>=1 && inputDate <=31){
+        if(inputMonth>=1 && inputMonth<=12){
+            if(isBirthPrime(inputDate)){
+                txt_Output.innerText = `Hey! ${inputName}\n DOB: ${inputDate}/${inputMonth}/${inputYear}\nYour birthday is not prime number!`
+            }else{
+                txt_Output.innerText = `Hey! ${inputName}\n DOB: ${inputDate}/${inputMonth}/${inputYear}\nYour birthday is prime number!`
+            }
+        }else{
+            var outputTxtNM = `You entered wrong month: ${inputMonth}`;
+            txt_Output.innerText = outputTxtNM;
+        }
+    }else{
+        var outputTxtND = `You entered wrong Date: ${inputDate}`;
+        txt_Output.innerText = outputTxtND;
     }
 }
 
